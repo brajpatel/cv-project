591268cd30ce48c6439c9d3763cd199d1d3931eb
brajpatel <brajeshpatel0002@gmail.com>
2023-03-17 09:54:22 +0000

    Change Education form to functional component

--- src/components/form components/Education.js
+++ src/components/form components/Education.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-class Education extends React.Component {
-    render() {
-        const { education, handleTitleOfStudy, handleSchoolName, handleStudyStart, handleStudyEnd, addEducation } = this.props;
+function Education(props) {
+    const { education, handleTitleOfStudy, handleSchoolName, handleStudyStart, handleStudyEnd, addEducation } = props;
 
-        return (
-            <div>
-                <h2>Educational Experience (up to 3)</h2>
-                <input onChange={(e) => handleTitleOfStudy(e)} value={education.title} placeholder="Title of Study" type="text"></input>
-                <input onChange={(e) => handleSchoolName(e)} value={education.school} placeholder="School Name" type="text"></input>
-                <h4>Start Date</h4>
-                <input onInput={(e) => handleStudyStart(e)} value={education.start} type="date"></input>
-                <h4>End Date</h4>
-                <input onInput={(e) => handleStudyEnd(e)} value={education.end} type="date"></input>
-                <button onClick={addEducation}>Add Education<i className="fa-solid fa-user-graduate"></i></button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Educational Experience (up to 3)</h2>
+            <input onChange={(e) => handleTitleOfStudy(e)} value={education.title} placeholder="Title of Study" type="text"></input>
+            <input onChange={(e) => handleSchoolName(e)} value={education.school} placeholder="School Name" type="text"></input>
+            <h4>Start Date</h4>
+            <input onInput={(e) => handleStudyStart(e)} value={education.start} type="date"></input>
+            <h4>End Date</h4>
+            <input onInput={(e) => handleStudyEnd(e)} value={education.end} type="date"></input>
+            <button onClick={addEducation}>Add Education<i className="fa-solid fa-user-graduate"></i></button>
+        </div>
+    )
 }
 
 export default Education;
