70b303037deb3dec458ffa78ce0157a74ac20676
brajpatel <brajeshpatel0002@gmail.com>
2023-03-17 10:03:48 +0000

    Change Preview Aside to functional component

--- src/components/preview components/Preview-Aside.js
+++ src/components/preview components/Preview-Aside.js
@@ -1,30 +1,28 @@
 import React from "react";
 
-class Aside extends React.Component {    
-    render() {
-        const { address, email, phone, skills, deleteSkill } = this.props;
+function Aside(props) {
+    const { address, email, phone, skills, deleteSkill } = props;
 
-        return (
-            <div className="preview-aside">
-                <div className="personal-details">
-                    <h4>Address</h4>
-                    <p>{address}</p>
-                    <h4>Email</h4>
-                    <p>{email}</p>
-                    <h4>Phone</h4>
-                    <p>{phone}</p>
-                </div>
-                <h3>Skills</h3>
-                <ul className="skills">
-                    {skills.map((skill) => {
-                        return (
-                            <li key={skill.id} className="skill">{skill.text}<i className="fa-solid fa-trash delete-skill" onClick={() => deleteSkill(skill.id)}></i></li>
-                        )
-                    })}
-                </ul>
+    return (
+        <div className="preview-aside">
+            <div className="personal-details">
+                <h4>Address</h4>
+                <p>{address}</p>
+                <h4>Email</h4>
+                <p>{email}</p>
+                <h4>Phone</h4>
+                <p>{phone}</p>
             </div>
-        )
-    }
+            <h3>Skills</h3>
+            <ul className="skills">
+                {skills.map((skill) => {
+                    return (
+                        <li key={skill.id} className="skill">{skill.text}<i className="fa-solid fa-trash delete-skill" onClick={() => deleteSkill(skill.id)}></i></li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Aside;
