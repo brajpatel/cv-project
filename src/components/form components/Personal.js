5abdd23cfbb1245be33eedb5ee45b30a9d825454
brajpatel <brajeshpatel0002@gmail.com>
2023-03-17 09:53:35 +0000

    Change Personal form to functional component

--- src/components/form components/Personal.js
+++ src/components/form components/Personal.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-class Personal extends React.Component {
-    render() {
-        const { handleTitle, handleForename, handleSurname, handleAddress, handleEmail, handlePhone, handleDescription } = this.props;
+function Personal(props) {
+    const { handleTitle, handleForename, handleSurname, handleAddress, handleEmail, handlePhone, handleDescription } = props;
 
-        return (
-            <div>
-                <h2>Personal Details</h2>
-                <input onChange={(e) => handleTitle(e)} placeholder="Title" type="text"></input>
-                <input onChange={(e) => handleForename(e)} placeholder="Forename" type="text"></input>
-                <input onChange={(e) => handleSurname(e)} placeholder="Surname" type="text"></input>
-                <input onChange={(e) => handleAddress(e)} placeholder="Address" type="text"></input>
-                <input onChange={(e) => handleEmail(e)} placeholder="Email" type="text"></input>
-                <input onChange={(e) => handlePhone(e)} placeholder="Phone Number" type="tel"></input>
-                <textarea onChange={(e) => handleDescription(e)} placeholder="Description (character limit: 300)" rows="4" maxLength="300"></textarea>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Personal Details</h2>
+            <input onChange={(e) => handleTitle(e)} placeholder="Title" type="text"></input>
+            <input onChange={(e) => handleForename(e)} placeholder="Forename" type="text"></input>
+            <input onChange={(e) => handleSurname(e)} placeholder="Surname" type="text"></input>
+            <input onChange={(e) => handleAddress(e)} placeholder="Address" type="text"></input>
+            <input onChange={(e) => handleEmail(e)} placeholder="Email" type="text"></input>
+            <input onChange={(e) => handlePhone(e)} placeholder="Phone Number" type="tel"></input>
+            <textarea onChange={(e) => handleDescription(e)} placeholder="Description (character limit: 300)" rows="4" maxLength="300"></textarea>
+        </div>
+    )
 }
 
 export default Personal;
