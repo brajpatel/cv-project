d23be9cc64dc0de954209cae0d48371356411852
brajpatel <brajeshpatel0002@gmail.com>
2023-03-17 09:52:38 +0000

    Change Work form to functional component

--- src/components/form components/Work.js
+++ src/components/form components/Work.js
@@ -1,23 +1,21 @@
 import React from "react";
 
-class Work extends React.Component {
-    render() {
-        const { job, handlePositionTitle, handleCompanyName, handlePositionDuties, handleWorkStart, handleWorkEnd, addWorkExperience } = this.props;
+function Work(props) {
+    const { job, handlePositionTitle, handleCompanyName, handlePositionDuties, handleWorkStart, handleWorkEnd, addWorkExperience } = props;
 
-        return (
-            <div>
-                <h2>Work Experience</h2>
-                <input onChange={(e) => handlePositionTitle(e)} value={job.position} placeholder="Position Title" type="text"></input>
-                <input onChange={(e) => handleCompanyName(e)} value={job.company} placeholder="Company Name" type="text"></input>
-                <textarea onChange={(e) => handlePositionDuties(e)} value={job.duties} placeholder="Main Tasks/Duties" rows="3" maxLength="200"></textarea>
-                <h4>Start Date</h4>
-                <input onInput={(e) => handleWorkStart(e)} value={job.start} type="date"></input>
-                <h4>End Date</h4>
-                <input onInput={(e) => handleWorkEnd(e)} value={job.end} type="date"></input>
-                <button onClick={addWorkExperience}>Add Work Experience<i className="fa-solid fa-briefcase"></i></button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Work Experience</h2>
+            <input onChange={(e) => handlePositionTitle(e)} value={job.position} placeholder="Position Title" type="text"></input>
+            <input onChange={(e) => handleCompanyName(e)} value={job.company} placeholder="Company Name" type="text"></input>
+            <textarea onChange={(e) => handlePositionDuties(e)} value={job.duties} placeholder="Main Tasks/Duties" rows="3" maxLength="200"></textarea>
+            <h4>Start Date</h4>
+            <input onInput={(e) => handleWorkStart(e)} value={job.start} type="date"></input>
+            <h4>End Date</h4>
+            <input onInput={(e) => handleWorkEnd(e)} value={job.end} type="date"></input>
+            <button onClick={addWorkExperience}>Add Work Experience<i className="fa-solid fa-briefcase"></i></button>
+        </div>
+    )
 }
 
 export default Work;
