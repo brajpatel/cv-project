861902999d3cbbfe72905f71cc3d9a9786bf1ffa
brajpatel <brajeshpatel0002@gmail.com>
2023-03-17 09:45:30 +0000

    Change Skills form to functional component

--- src/components/form components/Skills.js
+++ src/components/form components/Skills.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-class Skills extends React.Component {
-    render() {
-        const { skill, handleSkill, addSkill } = this.props;
+function Skills(props) {
+    const { skill, handleSkill, addSkill } = props;
 
-        return (
-            <div>
-                <h2>Skills (up to 12)</h2>
-                <input onChange={(e) => handleSkill(e)} value={skill.text} placeholder="Skill Name" type="text"></input>
-                <button onClick={addSkill}>Add Skill<i className="fa-solid fa-pencil"></i></button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Skills (up to 12)</h2>
+            <input onChange={(e) => handleSkill(e)} value={skill.text} placeholder="Skill Name" type="text"></input>
+            <button onClick={addSkill}>Add Skill<i className="fa-solid fa-pencil"></i></button>
+        </div>
+    )
 }
 
 export default Skills;
